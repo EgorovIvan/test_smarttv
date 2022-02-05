3f05484285f5399d4891735763729de73bdde06f
EgorovIvan <sportsmenq@mail.ru>
2022-02-05 11:35:36 +0500

    comment

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import Main from "./UI/Main";
+import "./UI/style/style.scss"
+
+
+function App() {
+    return (
+        <div className="wrapper">
+
+            {/*Компонент основной страницы*/}
+            <Main/>
+        </div>
+    );
+}
+
+export default App;
